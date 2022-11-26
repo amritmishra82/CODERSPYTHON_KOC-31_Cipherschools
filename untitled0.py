66a2b06987b185d7a6133020c17ee9284d85f709
amritmishra82 <115808810+amritmishra82@users.noreply.github.com>
2022-11-26 19:24:45 +0530

    Add files via upload

---
+++ untitled0.py
@@ -0,0 +1,8 @@
+n = int(input("How many Emails you wants to Slice: ")) 
+z = [] 
+for i in range (1,n+1): 
+ n = str(input("\nEmail: ")) 
+ z.append(n) 
+for i in z: 
+ m = i.split("@") 
+ print(f'\nUsername: {m[0]} and domain: {m[1]}')
